556b8321d4fc147f8f8a5ae83f5c2715d4c30d65
furenku <furenku@gmail.com>
2016-04-06 01:53:32 -0500

    wf home movil completo

--- footer.php
+++ footer.php
@@ -4,9 +4,30 @@
 
       <footer id="footer" class="columns h_10vh">
          <div class="vcenter row p2">
-            <div class="columns text-center"><i class="fa fa-facebook columns fontXL"></i><span class="columns fontS p2">TExto</span></div>
-            <div class="columns text-center"><i class="fa fa-facebook columns fontXL"></i><span class="columns fontS p2">TExto</span></div>
-            <div class="columns text-center"><i class="fa fa-facebook columns fontXL"></i><span class="columns fontS p2">TExto</span></div>
+            <div class="columns text-center">
+               <a href="#" target="_blank">
+                  <i class="fa fa-facebook columns fontXL"></i>
+                  <span class="columns fontS p2">Facebook</span>
+               </a>
+            </div>
+            <div class="columns text-center">
+               <a href="#" target="_blank">
+                  <i class="fa fa-twitter columns fontXL"></i>
+                  <span class="columns fontS p2">Twitter</span>
+               </a>
+            </div>
+            <div class="columns text-center">
+               <a href="#" target="_blank">
+                  <i class="fa fa-instagram columns fontXL"></i>
+                  <span class="columns fontS p2">Instagram</span>
+               </a>
+            </div>
+            <div class="columns text-center">
+               <a href="#" target="_blank">
+                  <i class="fa fa-envelope columns fontXL"></i>
+                  <span class="columns fontS p2">E-mail</span>
+               </a>
+            </div>
          </div>
       </footer>
 
