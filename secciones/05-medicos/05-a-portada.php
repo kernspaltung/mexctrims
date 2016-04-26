0d7a31b4ef09a60dbad80494daeea339fe361a82
josecaos <jazzvoon@gmail.com>
2016-04-26 08:53:24 -0500

    portada medicos, archive

---
+++ secciones/05-medicos/05-a-portada.php
@@ -0,0 +1,119 @@
+<div class="h_100">
+  <div class="medium-12 large-12 columns h_20 p0">
+    <!-- (.large-5.columns.h_100>(.large-4.columns.h_100>.h_100.imgLiquid.imgLiquidFill>img[src="http://fakeimg.pl/600x300"])+.large-8.columns.h_100>.large-12.columns.fontXL.black.uppercase.vcenter{Sección Medicos})+.large-7.columns.h_100>.large-12.columns.h_50.fontXS+.large-9.columns.h_50.fontXS+button.large-3.columns.button.hollow.cursor-pointer.h_50.fontS.uppercase{Contribución} -->
+    <div class="large-5 columns h_100">
+      <div class="large-4 columns h_100 p0">
+        <div class="h_100 imgLiquid imgLiquidFill"><img src="http://fakeimg.pl/600x300" alt=""></div>
+      </div>
+      <div class="large-8 columns h_100">
+        <div class="large-12 columns fontXL black uppercase vcenter">Sección Medicos</div>
+      </div>
+    </div>
+    <div class="large-7 columns h_100">
+      <div class="large-12 columns h_50">
+        <div class="large-12 fontS black vcenter text-left">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis enim in illo excepturi illum optio, quo perspiciatis dolor doloribus, ratione, non libero nobis possimus.
+        </div>
+      </div>
+      <div class="large-9 columns h_50">
+        <div class="large-12 fontS black vcenter text-left">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </div>
+      </div>
+      <button class="large-3 columns button hollow cursor-pointer h_50 fontS uppercase">Contribución</button>
+    </div>
+  </div>
+  <div class="large-12 columns h_60">
+    <!-- (.large-5.columns.h_100>.h_100.imgLiquid.imgLiquidFill>img[src="http://fakeimg.pl/600x300"])+.large-7.columns.h_100{lorem10} -->
+    <div class="large-5 columns h_100 p0">
+      <div class="h_100 imgLiquid imgLiquidFill">
+        <img src="http://fakeimg.pl/600x300" alt="">
+      </div>
+    </div>
+    <div class="large-7 columns h_100">
+      <!-- .large-12.columns.h_50*2 -->
+      <div class="large-12 columns h_50">
+        <!-- .large-12.columns.h_40+.large-12.columns.h_60   -->
+        <div class="large-12 columns h_40">
+          <!-- h1.large-12.fontL.black.text-center.vcenter -->
+          <h1 class="large-12 fontXL black text-center vcenter">Titulo de la eeeeentrrrrrraaadaa (laaaaaargo)</h1>
+        </div>
+        <div class="large-12 columns h_60">
+          <!-- (.large-6.columns.h_100.text-center>.large-12.columns.fontM.subheader.vcenter{Subtitulo entrada})+.large-6.columns.h_100>(.large-8.columns.h_100>(.large-12.columns.h_60>.large-12.fontM.black.vcenter)+.large-12.columns.h_40>.large-12.fontXS.subheader.vcenter{YYYY/MM/DD})+.large-4.columns.h_100.p0>.h_100.imgLiquid.imgLiquidFill>img[src="http://fakeimg.pl/100x100"] -->
+          <div class="large-6 columns h_100 text-center">
+            <div class="large-12 columns fontL subheader vcenter">Subtitulo entrada</div>
+          </div>
+          <div class="large-6 columns h_100">
+            <div class="large-8 columns h_100">
+              <div class="large-12 columns h_60">
+                <div class="large-12 fontM black vcenter">Dr. Mijau Sancar</div>
+              </div>
+              <div class="large-12 columns h_40">
+                <div class="large-12 fontXS subheader vcenter">YYYY/MM/DD</div>
+              </div>
+            </div>
+            <div class="large-4 columns h_100 p0">
+              <div class="h_100 imgLiquid imgLiquidFill"><img src="http://fakeimg.pl/100x100" alt=""></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="large-12 columns h_50">
+        <!-- (.large-12.columns.h_60>.large-12.fontS.black.vcenter{lorem22})+.large-12.columns.h_40>(.large-8.columns.h_100)+.large-4.columns.h_100>.large-6.fontL.black.vcenter{Leer más}+.large-6.columns.fontXL.black.vcenter>i.fa.fa-angle-right -->
+        <div class="large-12 columns h_60">
+          <div class="large-12 fontS black vcenter text-left">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque quod optio maiores, totam velit voluptas? Quo aperiam quidem amet aliquid quae tempore.
+          </div>
+        </div>
+        <div class="large-12 columns h_40">
+          <div class="large-8 columns h_100">
+            <!-- (.large-12.columns.h_30>.large-12.bold.fontM.black.vcenter{Comentarios (12)})+.large-12.columns.h_70>.large-12.bold.fontM.black.vcenter{lorem13} -->
+            <div class="large-12 columns h_30">
+              <div class="large-12 bold fontM black vcenter">Comentarios (12)</div>
+            </div>
+            <div class="large-12 columns h_70">
+              <div class="large-12 subheader fontS text-left black vcenter">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, dolorem, autem. At, fuga.
+              </div>
+            </div>
+          </div>
+          <button class="button hollow pointer large-4 columns h_100">
+            <div class="large-9 columns fontS black vcenter">Leer más</div>
+            <div class="large-3 columns fontXL black vcenter"><i class="fa fa-angle-right"></i></div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="large-12 columns minH_20">
+    <!-- .row.large-4.h_20vh>(.large-12.columns.h_30>.large-12.fontM.bold.black.vcenter{Titulo Entrada})+.large-12.columns.h_70>((.large-9.columns.h_100>(.large-12.columns.h_50>.large-12.fontS.bold.vcenter{Autor})+.large-12.columns.h_50>.large-12.fontS.bold.vcenter{YYYY/MM/DD})+.large-3.columns.h_100>.button.hollow.h_100.w_100>i.fa.fa-angle-right) -->
+    <?php
+    $html = "";
+    for ($i=0; $i < 3; $i++) {
+      $html .= '
+
+    <div class="large-4 column h_100 vcenter">
+      <div class="large-12 columns h_40">
+        <div class="large-12 fontM bold black vcenter">Titulo Entrada</div>
+      </div>
+      <div class="large-12 columns h_60">
+        <div class="large-9 columns h_100">
+          <div class="large-12 columns h_50">
+            <div class="large-12 text-left fontS bold vcenter">Autor</div>
+          </div>
+          <div class="large-12 columns h_50">
+            <div class="large-12 text-left fontS bold vcenter">YYYY/MM/DD</div>
+          </div>
+        </div>
+        <div class="large-3 columns h_100">
+          <div class="button hollow h_100 w_100">
+          <i class="fa fa-angle-right"></i></div>
+        </div>
+      </div>
+    </div>';
+    }
+    echo $html;
+    ?>
+
+  </div>
+</div>
