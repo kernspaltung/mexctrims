0176d211e9bdb55a92e99cae6da5eef2b017fdde
josecaos <jazzvoon@gmail.com>
2016-04-28 01:56:59 -0500

    nuevo archivo medicos

--- 05-medicos.php
+++ 05-medicos.php
@@ -3,6 +3,7 @@
 
 get_header(); ?>
 
-<?php include_once 'secciones/05-medicos/05-a-portada.php'; ?>
+<?php #include_once 'secciones/05-medicos/05-a-portada.php'; ?>
+<?php include_once 'secciones/05-medicos/05-b-single.php'; ?>
 
 <?php get_footer(); ?>
---
+++ secciones/05-medicos/05-b-single.php
@@ -0,0 +1,25 @@
+<div class="h_100">
+  <!-- .large-5.columns.h_100+.large-7.columns.h_100 -->
+  <div class="medium-12 large-6 columns h_100">
+    <div class="large-12 columns h_30vh">
+      <!-- titulo -->
+      <div class="large-12 fontXXL black p0 vcenter">Titulo entrada Completo largo posiblemente</div>
+    </div>
+    <div class="large-12 columns h_70vh p0">
+      <div class="h_100 imgLiquid imgLiquidFill">
+        <img src="http://fakeimg.pl/400x600" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="medium-12 large-6 columns h_100">
+    <div class="large-12 columns h_30vh">
+      <!-- titulo -->
+      <div class="large-12 fontXXL black p0 vcenter">Titulo entrada Completo largo posiblemente</div>
+    </div>
+    <div class="large-12 columns h_70vh p0">
+      <div class="h_100 imgLiquid imgLiquidFill">
+        <img src="http://fakeimg.pl/400x600" alt="">
+      </div>
+    </div>
+  </div>
+</div>
