dca714f28f9d237d2fbe1800fc62802ff6b2ad01
josecaos <jazzvoon@gmail.com>
2016-04-28 02:53:57 -0500

    pagina medicos single wire listo

--- secciones/05-medicos/05-b-single.php
+++ secciones/05-medicos/05-b-single.php
@@ -11,15 +11,52 @@
       </div>
     </div>
   </div>
-  <div class="medium-12 large-6 columns h_100">
+  <div class="medium-12 large-6 columns h_100 p0">
     <div class="large-12 columns h_30vh">
-      <!-- titulo -->
-      <div class="large-12 fontXXL black p0 vcenter">Titulo entrada Completo largo posiblemente</div>
+      <div class="large-8 columns h_100">
+        <div class="large-12 columns h_30 p0">
+          <div class="large-12 fontXL text-left black p0 vcenter">Dr. Kernspaltung</div>
+        </div>
+        <div class="large-12 columns h_50 p0">
+          <div class="large-12 fontL text-left black p0 vcenter">
+            Lorem ipsum dolor sit amet, opio modi obcaecati libero dolore repellendus blanditiis ut inventore, quidem?
+          </div>
+        </div>
+        <div class="large-12 columns h_20 p0">
+          <div class="large-12 text-left fontM black p0 vcenter">YYY/MM/DD</div>
+        </div>
+      </div>
+      <div class="large-4 columns h_100 p0">
+        <!-- .large-12.columns.h_80>.h_100.imgLiquid.imgLiquidFill>img[src="http://fakeimg.pl/Kern"]+.large-12.columns.h_20>.large-12.columns.h_20.fontS.black.p0.vcenter{Ver perfil}+i.fa.fa-angle-right -->
+        <div class="large-12 columns h_80 p0">
+          <div class="h_100 imgLiquid imgLiquidFill">
+            <img src="http://fakeimg.pl/100x100" alt="">
+          </div>
+        </div>
+        <a href="#" class="large-12 columns h_20 button hollow">
+          <div class="large-12 columns h_20 fontM black p0 vcenter">Ver perfil&nbsp;&nbsp;&nbsp;<i class="fa fa-angle-right"></i></div>
+        </a>
+      </div>
     </div>
-    <div class="large-12 columns h_70vh p0">
-      <div class="h_100 imgLiquid imgLiquidFill">
-        <img src="http://fakeimg.pl/400x600" alt="">
+    <div class="large-12 columns minH_70vh p0">
+      <!-- (.large-12.columns.h_20>.large-12.fontXXL.subtitle.bold.black.text-left{subtitulo})+.large-12.columns.minH_60>.large-12.fontL.black.text-left{lorem32} -->
+      <div class="large-12 columns h_20">
+        <div class="large-12 fontXXL subheader bold text-left p4">subtitulo</div>
+      </div>
+      <div class="large-12 columns minH_60 p4">
+        <div class="large-12 fontL black text-left mb2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates neque cum error ea enim impedit soluta quis debitis, eligendi, dignissimos quasi, laboriosam quam quas at omnis hic maiores facere natus temporibus deleniti voluptas vero! Necessitatibus amet pariatur quod, obcaecati impedit harum qui asperiores repellat maiores, maxime, quidem facilis. Repellendus eaque delectus qui obcaecati.
+        </div>
+        <div class="large-12 columns h_40vh p0 mb2">
+          <div class="h_100 imgLiquid imgLiquidFill">
+            <img src="http://fakeimg.pl/320x420" alt="">
+          </div>
+        </div>
+        <div class="large-12 fontL black text-left mb2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates neque cum error ea enim impedit soluta quis debitis, eligendi, dignissimos quasi, laboriosam quam quas at omnis hic maiores facere natus temporibus deleniti voluptas vero! Necessitatibus amet pariatur quod, obcaecati impedit harum qui asperiores repellat maiores, maxime, quidem facilis. Repellendus eaque delectus qui obcaecati.
+        </div>
       </div>
     </div>
   </div>
+  <?php include_once 'secciones/05-medicos/05-c-comentarios.php'; ?>
 </div>
---
+++ secciones/05-medicos/05-c-comentarios.php
@@ -0,0 +1,4 @@
+<div class="h_100">
+  <!-- .large-5.columns.h_100+.large-7.columns.h_100 -->
+
+</div>
