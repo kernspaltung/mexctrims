1b78f1bc17e7db00c1ebf23448ed454721c3fef1
furenku <furenku@gmail.com>
2016-05-05 23:12:19 -0500

    faltantes

---
+++ single.php
@@ -0,0 +1,84 @@
+<?php
+
+get_header();
+
+?>
+
+
+<div class="columns h_100 p0">
+
+
+<?php get_template_part( 'secciones/00-elementos/content','02-taxonomias' ); ?>
+
+
+
+  <!-- contenido -->
+  <article id="single-contenedor-contenido" class="small-12 large-8 columns scrollH">
+
+   <?php get_template_part( 'secciones/00-elementos/content','03-taxonomias_acordeon' ); ?>
+
+
+    <div id="single-contenido-entrada" class="small-12 columns p6 text-left">
+
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam repudiandae quaerat nulla nobis eum inventore est ex. Delectus ab perspiciatis deserunt dolore?</p>
+        <p>Rerum nobis sed cupiditate tenetur voluptates, delectus cum cumque molestias ducimus maxime omnis, neque quaerat id quod aperiam dolorum commodi voluptatum velit.</p>
+
+
+        <img src="http://fakeimg.pl/500x420" alt="">
+
+
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam repudiandae quaerat nulla nobis eum inventore est ex. Delectus ab perspiciatis deserunt dolore?</p>
+       <p>Rerum nobis sed cupiditate tenetur voluptates, delectus cum cumque molestias ducimus maxime omnis, neque quaerat id quod aperiam dolorum commodi voluptatum velit.</p>
+
+
+       <img src="http://fakeimg.pl/500x420" alt="">
+
+
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam repudiandae quaerat nulla nobis eum inventore est ex. Delectus ab perspiciatis deserunt dolore?</p>
+        <p>Rerum nobis sed cupiditate tenetur voluptates, delectus cum cumque molestias ducimus maxime omnis, neque quaerat id quod aperiam dolorum commodi voluptatum velit.</p>
+
+
+        <img src="http://fakeimg.pl/500x420" alt="">
+
+
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam repudiandae quaerat nulla nobis eum inventore est ex. Delectus ab perspiciatis deserunt dolore?</p>
+       <p>Rerum nobis sed cupiditate tenetur voluptates, delectus cum cumque molestias ducimus maxime omnis, neque quaerat id quod aperiam dolorum commodi voluptatum velit.</p>
+
+
+       <img src="http://fakeimg.pl/500x420" alt="">
+
+
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam repudiandae quaerat nulla nobis eum inventore est ex. Delectus ab perspiciatis deserunt dolore?</p>
+        <p>Rerum nobis sed cupiditate tenetur voluptates, delectus cum cumque molestias ducimus maxime omnis, neque quaerat id quod aperiam dolorum commodi voluptatum velit.</p>
+
+
+        <img src="http://fakeimg.pl/500x420" alt="">
+
+
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam repudiandae quaerat nulla nobis eum inventore est ex. Delectus ab perspiciatis deserunt dolore?</p>
+       <p>Rerum nobis sed cupiditate tenetur voluptates, delectus cum cumque molestias ducimus maxime omnis, neque quaerat id quod aperiam dolorum commodi voluptatum velit.</p>
+
+
+       <img src="http://fakeimg.pl/500x420" alt="">
+
+
+
+
+    </div>
+
+  </article>
+  <!-- contenido -->
+
+
+
+
+
+</div>
+<?php
+
+get_footer();
+
+?>
