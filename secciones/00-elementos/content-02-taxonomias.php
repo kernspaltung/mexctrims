136d657a0cffd1d0272089209976c0d0fd2d7376
furenku <furenku@gmail.com>
2016-05-19 15:40:57 -0500

    integrar taxonomias a single sidebar

--- secciones/00-elementos/content-02-taxonomias.php
+++ secciones/00-elementos/content-02-taxonomias.php
@@ -1,34 +1,54 @@
 <!-- Categorias large -->
 <aside id="noticias-aside-large" class="h_100 large-4 columns show-for-large ha" data-sticky-container>
- <div class="sticky h_75vh fontM" data-sticky data-anchor="plantillas" data-margin-top="9">
+   <div class="sticky h_75vh fontM" data-sticky data-anchor="plantillas" data-margin-top="9">
 
-  <h2 class="columns fontL p3 text-center">Categorias</h2>
+      <h2 class="columns fontL p3 text-center">Categorias</h2>
 
-  <ul id="noticias-tags-lista" class="menu small-12 columns p0 h_20vh scrollH">
-    <?php
-    for ($i=0; $i < 12; $i++) {
-     echo  '<a href="">
-        <li class="small-4 columns fontXXS p3 text-center">Categoría' . $i . '</li>
-     </a>';
-    }
-    ?>
-  </ul>
+      <ul id="noticias-tags-lista" class="menu small-12 columns p0 h_20vh scrollH">
+         <?php
+
+         $medicos_destacada = get_term_by('name','Destacada Médicos', 'category' );
+         $medicos = get_term_by('name','Médicos', 'category' );
+         $noticia_principal = get_term_by('name','Noticia Principal', 'category' );
+         $noticias = get_term_by('name','Noticias', 'category' );
+         $categorias_excluidas  = array( 1, $medicos_destacada -> term_id, $medicos->term_id, $noticia_principal->term_id, $noticias->term_id, );
+         if( get_the_category() )
+         foreach( get_the_category() as $categoria ) :
+            if( ! in_array( $categoria->term_id, $categorias_excluidas ) ):
+               ?>
+               <a href="<?php echo get_category_link( $categoria -> term_id ); ?>">
+                  <li class="small-4 columns fontXXS p3 text-center">
+                     <?php echo $categoria->name; ?>
+                  </li>
+               </a>
+               <?php
+            endif;
+         endforeach;
+         ?>
+      </ul>
 
 
-<!-- Etiquetas large -->
-  <h2 class="columns fontL p3 text-center">Etiquetas</h2>
+      <!-- Etiquetas large -->
+      <h2 class="columns fontL p3 text-center">Etiquetas</h2>
 
-  <ul id="noticias-tags-lista" class="menu small-12 columns p0 h_20vh scrollH">
-    <?php
-    for ($i=0; $i < 24; $i++) {
-     echo  '<a href="">
-        <li class="small-4 columns fontXXS p3 text-center">Etiqueta' . $i . '</li>
-     </a>';
-    }
-    ?>
-  </ul>
+      <ul id="noticias-tags-lista" class="menu small-12 columns p0 h_20vh scrollH">
+         <?php
+         if( get_the_tags() )
+         foreach( get_the_tags() as $etiqueta ) :
+            ?>
 
- </div>
+            <a href="<?php echo get_tag_link( $etiqueta->term_id); ?>">
+               <li class="small-4 columns fontXXS p3 text-center">
+                  <?php echo $etiqueta->name; ?>
+               </li>
+            </a>
+
+            <?php
+         endforeach;
+         ?>
+      </ul>
+
+   </div>
 </aside>
 <!-- Etiquetas large -->
 <!-- Categorias large -->
