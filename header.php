e5eeb234eff35f5bfa789298994ea8ddc818a6d5
furenku <furenku@gmail.com>
2016-05-20 14:17:50 -0500

    subrayado header

--- header.php
+++ header.php
@@ -11,7 +11,23 @@
 </head>
 <body>
 
-   <header id="header" class="small-12 h_15vh h_sm_25vh p0 fixed white_bg z1k1">
+   <?php
+   $color_orilla = 'neutral_bd';
+   if( is_page('Inicio') )
+      $color_orilla = 'transparente_bd';
+   if( is_page('Información') )
+      $color_orilla = 'secundario1_bd';
+   if( is_page('Noticias') )
+      $color_orilla = 'secundario2_bd';
+   if( is_page('Descargables') )
+      $color_orilla = 'secundario3_bd';
+   if( is_page('Médicos') )
+      $color_orilla = 'primario_bd';
+   if( is_page('Regístrate') )
+      $color_orilla = 'primario_acento_bd';
+   ?>
+
+   <header id="header" class="small-12 h_15vh h_sm_25vh p0 fixed white_bg z1k1 <?php echo $color_orilla; ?>">
 
       <div id="logo" class="small-6 medium-3 large-2 columns h_100 h_sm_50 p0">
          <a href="<?php echo site_url(); ?>" class="button white_bg black small-12 h_15vh m0 p0">
@@ -25,14 +41,14 @@
 
       <div id="header_quicklinks" class="small-6 medium-3 columns h_sm_50 medium-push-6 large-push-7 text-right p0">
 
-         <div id="area_medicos" class="small-6 medium-4 columns small-text-center medium-text-right h_100 secundario1 txsh1">
+         <div id="area_medicos" class="small-6 medium-4 columns small-text-center medium-text-right h_100">
 
             <?php $page = get_page_by_title('Médicos'); ?>
 
             <a href="<?php echo get_the_permalink( $page->ID ); ?>" class="button white_bg black small-12 h_15vh m0 p0">
                <div class="vcenter">
-                  <i class="fa fa-user-md fontXL p2"></i>
-                  <h6 class="fontS"><?php echo get_the_title( $page->ID ); ?></h6>
+                  <i class="fa fa-user-md fontXL p2 primario"></i>
+                  <h6 class="fontS neutral"><?php echo get_the_title( $page->ID ); ?></h6>
                </div>
             </a>
          </div>
@@ -74,11 +90,11 @@
                $icono = $iconos[$i];
                ?>
 
-               <li class="small-3 columns w_100 text-center h_100 <?php echo $colores[$i]; ?>">
+               <li class="small-3 columns w_100 text-center h_100">
                   <a href="<?php echo get_the_permalink( $page->ID ); ?>" class="button white_bg black small-12 h_100 m0 p0"> <!-- antes h_15vh -->
                      <div class="vcenter">
-                        <i class="fa fa-<?php echo $icono; ?> fontXL p2"></i>
-                        <div><?php echo get_the_title( $page->ID ); ?></div>
+                        <i class="fa fa-<?php echo $icono; ?> fontXL p2 <?php echo $colores[$i]; ?>"></i>
+                        <div class="neutral"><?php echo get_the_title( $page->ID ); ?></div>
                      </div>
                   </a>
                </li>
