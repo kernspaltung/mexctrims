c88079da6060a309373fd752835323c67a7ef24d
furenku <furenku@gmail.com>
2016-09-24 05:52:22 -0500

    añadir espacio para twitter y reestructuracion noticias home

--- secciones/01-inicio/content-01-portada.php
+++ secciones/01-inicio/content-01-portada.php
@@ -15,23 +15,23 @@ $bienvenida = get_page_by_title('Bienvenida');
    <div class="img-overlay w_100 h_100 z-1 p0 abs"></div>
 </div>
 
-<div class="h_100 text-center rel z1k">
+<div class="h_100 rel z1k">
    <div class="row p4 h_75">
-      <div class="small-12 medium-6 large-7 columns h_30vh h_sm_20vh">
+      <div class="small-12 medium-6 large-5 columns h_40vh h_sm_20vh">
          <div class="h_100 imgLiquid imgLiquidFill image-fx">
 
             <?php echo get_the_post_thumbnail( $bienvenida -> ID ); ?>
 
          </div>
       </div>
-      <div class="small-12 medium-6 large-5 columns fontL h_30vh h_sm_55">
-         <div class="vcenter text-center">
+      <div class="small-12 medium-6 large-7 columns fontL h_40vh h_sm_55">
+         <div class="vcenter">
 
-            <div class="mt_sm_2 imgLiquid imgLiquidNoFill h_15vh">
+            <div class="mt_sm_2 imgLiquid imgLiquidNoFill h_25vh">
                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logotipo-150w-2x.png">
             </div>
 
-            <h6 class="mt1 mt_sm_0 fontL text-center p5 pt0 pb0">
+            <h6 class="mt1 mt_sm_0 fontL text-center p2 pt0 pb0">
                Comité Mexicano para el
                <br/>Tratamiento e Investigación
                <br/>en Esclerosis Múltiple
@@ -39,10 +39,10 @@ $bienvenida = get_page_by_title('Bienvenida');
             </h6>
          </div>
       </div>
-      <div class="small-12 medium-10 large-8 medium-offset-1 large-offset-2 columns pt4 pt_sm_1">
-         <h4 class="fontXL font_sm_L p3 text-left">
+      <div class="small-12 medium-10 large-8 medium-offset-1 large-offset-2 columns p5 pt_sm_1 mt3">
+         <h6 class="fontXL font_sm_L p5 text-left ml3 mr3 white_bg tarjeta">
             <?php echo apply_filters('the_content', $bienvenida -> post_content ); ?>
-         </h4>
+         </h6>
       </div>
    </div>
 
--- secciones/01-inicio/content-02-noticias.php
+++ secciones/01-inicio/content-02-noticias.php
@@ -1,63 +1,73 @@
-<section id="noticias" class="row mt4 mt_sm_0 mb4 mb_sm_1 p5 p_sm_2">
+<section id="noticias" class="row  mt_sm_0 mb4 mb_sm_1 p5 p_sm_2">
 
-   <h1 class="small-12 small-text-center medium-text-left p0 mb1 secundario2_bg p4 white">
+   <h3 class="small-12 small-text-center medium-text-left p0 mb1 secundario2_bg white p2 ">
       Noticias
-   </h1>
+   </h3>
 
-   <?php
+   <section id="twitter" class="columns hide-for-small-only medium-6 h_70vh white_bg mt1">
+      <div class="vcenter columns small-6 small-offset-3 end text-center p5 primario_acento_bd tarjeta">
+         <div class="columns">
+            proximamente:
+         </div>
+         <div class="columns">
+            twitter
+         </div>
+         <div class="columns">
+            @mexctrims
+         </div>
+         <div class="columns text-center fontL">
+            <i class="fa fa-twitter"></i>
+         </div>
+      </div>
+   </section>
 
-   $destacada_inicio = get_term_by( 'name', 'Noticia Principal', 'category' );
+   <section id="noticias" class="columns hide-for-small-only medium-6 h_60vh ">
 
-   $q = new WP_Query( array( 'post_type'=>'post', 'posts_per_page'=>2, 'cat' => $destacada_inicio->term_id) );
+      <?php
 
-   if( $q -> have_posts() ) :
-      while( $q -> have_posts() ) :
-         $q -> the_post();
-      ?>
+      $destacada_inicio = get_term_by( 'name', 'Noticia Principal', 'category' );
 
-      <section id="noticia_destacada" class="small-12 medium-7 large-8 h_60vh h_sm_70vh mt1 mb1 mb_sm_0  p3">
-         <a href="<?php echo get_the_permalink(get_the_ID()); ?>" class="h_100 tarjeta  white_bg p3 ">
+      $q = new WP_Query( array( 'post_type'=>'post', 'posts_per_page'=>2, 'cat' => $destacada_inicio->term_id) );
 
-            <h3 class="p2 mb1  fontXL font_sm_M small-text-center medium-text-left">
-               <?php echo apply_filters( 'the_title', get_the_title() ); ?>
-            </h3>
+      if( $q -> have_posts() ) :
+         while( $q -> have_posts() ) :
+            $q -> the_post();
+            ?>
 
-            <div class="small-12 medium-6 columns imagen h_60 h_sm_20 imgLiquid imgLiquidFill">
-               <?php echo get_the_post_thumbnail( get_the_ID() ); ?>
-            </div>
+            <section id="noticia_destacada" class="columns h_35vh h_sm_70vh m0  p3">
+               <a href="<?php echo get_the_permalink(get_the_ID()); ?>" class="h_100 tarjeta  white_bg p4 ">
 
+                  <h6 class="p0 mb1  fontL font_sm_M small-text-center medium-text-left">
+                     <?php echo apply_filters( 'the_title', get_the_title() ); ?>
+                  </h6>
 
-            <div class="extracto_fecha small-12 medium-6 columns p0 text-left h_80 h_sm_35">
-               <div class="fecha small-12 columns text-right p2 p_sm_3 pt_sm_1 pb_sm_1 fontS">
-                  Publicada el <?php echo get_the_date('d \d\e F\, Y',get_the_ID()); ?>
-               </div>
-               <div class="p4 p_sm_3 pt0 pb0 fontM font_sm_S ">
-                  <?php echo apply_filters( 'the_excerpt', wp_trim_words( get_the_excerpt(), 12 ) ); ?>
-               </div>
-
-               <div class="small-12 columns h_20 text-right p5 p_sm_0">
-
-                  <div class="button fontS mt2 mt_sm_0">
-                     Ver Más
-                     <i class="fa fa-plus"></i>
+                  <div class="fecha columns p0 pb1 p_sm_3 pt_sm_1 pb_sm_1 fontS text-left">
+                     Publicada el <?php echo get_the_date('d \d\e F\, Y',get_the_ID()); ?>
                   </div>
 
-               </div>
+                  <div class="imagen small-12 medium-6 columns imagen h_50 h_sm_20 imgLiquid imgLiquidFill">
+                     <?php echo get_the_post_thumbnail( get_the_ID() ); ?>
+                  </div>
 
-            </div>
+                  <div class="extracto_fecha small-12 medium-6 columns p0 text-left h_80 h_sm_35">
+                     <div class="columns p3 p_sm_3 pt0 pb0 fontM font_sm_S ">
+                        <?php echo apply_filters( 'the_excerpt', wp_trim_words( get_the_excerpt(), 12 ) ); ?>
+                     </div>
 
-         </a>
-      </section>
+                  </div>
 
-   <?php
-      endwhile;
-   endif;
-   ?>
+               </a>
+            </section>
+
+            <?php
+         endwhile;
+      endif;
+      ?>
 
 
 
 
-      <section id="inicio-noticias-medium" class="medium-5 large-4 columns p5 pt0 m0 text-center rel hide-for-small-only">
+      <section id="inicio-noticias-medium" class="columns p0 pt0 m0 text-center rel hide-for-small-only">
 
 
          <?php
@@ -68,25 +78,23 @@
                $q -> the_post();
                // echo apply_filters( 'the_title', get_the_title() );
 
-         ?>
+               ?>
 
-         <article class="noticia_destacada_secundaria_inicio small-12 columns h_30vh mb1 p3">
+               <article class="noticia_destacada_secundaria_inicio small-12 medium-6 columns h_30vh p3 pt0">
 
-            <a href="<?php echo get_the_permalink(get_the_ID()); ?>" class="h_100 tarjeta white_bg">
-               <h3 class="p2 m0 fontM font_sm_S text-left">
-                  <?php echo apply_filters( 'the_title', get_the_title() ); ?>
-               </h3>
-               <div class="small-12 columns imagen h_10vh imgLiquid imgLiquidFill">
-                  <?php echo get_the_post_thumbnail( get_the_ID() ); ?>
-               </div>
-               <div class="fecha small-12 columns text-right p2 p_sm_3 pt_sm_1 pb_sm_1 fontS">
-                  Publicada el <?php echo get_the_date('d \d\e F\, Y',get_the_ID()); ?>
-               </div>
-               <div class="extracto small-12 columns p0 text-left h_25 fontS">
-                  <?php echo apply_filters( 'the_excerpt', wp_trim_words( get_the_excerpt(), 8 ) ); ?>
-               </div>
-               <!-- <div class="w_100 text-right p5 pt0">
-                  <div class="button fontS m0">
+                  <a href="<?php echo get_the_permalink(get_the_ID()); ?>" class="h_100 tarjeta white_bg p3">
+                     <h3 class="p0 pb1 m0 fontM font_sm_S text-left">
+                        <?php echo apply_filters( 'the_title', get_the_title() ); ?>
+                     </h3>
+                     <div class="small-12 columns imagen h_10vh imgLiquid imgLiquidFill ">
+                        <?php echo get_the_post_thumbnail( get_the_ID() ); ?>
+                     </div>
+                     <div class="fecha columns text-right p0 p_sm_3 pt_2 pb_sm_1 fontXS">
+                        Publicada el <?php echo get_the_date('d \d\e F\, Y',get_the_ID()); ?>
+                     </div>
+
+                     <!-- <div class="w_100 text-right p5 pt0">
+                     <div class="button fontS m0">
                      Ver Más
                      <div class="fa fa-plus"></div>
                   </div>
@@ -96,11 +104,23 @@
          </article>
 
          <?php
-            endwhile;
-         endif;
-         ?>
+      endwhile;
+   endif;
+   ?>
 
-      </section>
+</section>
 
+
+   <div class="columns h_10 text-right p2 pr1 m0">
+
+      <a href="<?php echo get_the_permalink( get_page_by_title("Noticias")->ID ); ?>">
+         <div class="button p3 primario_acento_bd fontS tarjeta primario_acento_bg_hover complementario_hover">
+            Ver Más
+            <i class="fa fa-plus"></i>
+         </div>
+      </a>
+
+   </div>
+</section>
 
 </section>
