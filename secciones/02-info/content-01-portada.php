31b271d41accc216118c1099e232dd713bf8fb95
furenku <furenku@gmail.com>
2017-05-25 02:49:27 -0500

    correcciones info movil

--- secciones/02-info/content-01-portada.php
+++ secciones/02-info/content-01-portada.php
@@ -14,7 +14,7 @@ $paginas = get_pages( array( 'child_of' => get_the_ID() ) );
 
 <!-- (h1>lorem9^+p>lorem10^+h2>lorem9^+(p>lorem18)*3)*4 -->
 
-<section id="info-menu" class="small-12 columns h_45vh mt_sm_2 ">
+<section id="info-menu" class="small-12 columns h_45vh h_sm_70vh mt_sm_2 pb_sm_4">
 
    <?php
    $iconos = array( 'neurona2', 'nodo', 'cerebro', 'red2' );
@@ -23,7 +23,7 @@ $paginas = get_pages( array( 'child_of' => get_the_ID() ) );
       $icono = $iconos[$i];
       ?>
 
-      <div class="small-4 w_20 columns h_50 h_sm_50 text-center p4 boton-info">
+      <div class="medium-6 large-4 w_20 columns h_50 h_sm_20 text-center p4 boton-info">
          <a href="<?php echo get_the_permalink( $pagina -> ID ); ?>" class="h_100 w_100 tarjeta secundario<?php echo ($i%3)+1; ?>_bg ">
             <div class="vcenter neutral_oscuro icono">
                <!-- <div class="w_100 h_5vh imgLiquid imgLiquidNoFill mb1">
