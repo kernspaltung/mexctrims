2b06c55a10efa55b5dcdeb73d55ec3fe77478c85
furenku <furenku@gmail.com>
2017-05-25 02:41:34 -0500

    correcciones stickies

--- secciones/00-elementos/content-06-enlaces_destacados.php
+++ secciones/00-elementos/content-06-enlaces_destacados.php
@@ -5,15 +5,21 @@ if( $q -> have_posts() ) :
       $q -> the_post();
 ?>
 
-      <div class="enlace_destacado small-12 h_25 p4">
+      <div class="enlace_destacado small-12 h_25 h_sm_50 p4">
+
          <a href="<?php echo get_post_meta( get_the_ID(), 'link-externo', true); ?>" target="_blank">
-            <div class="imagen imgLiquid imgLiquidNoFill h_70 tarjeta">
-               <?php echo get_the_post_thumbnail(); ?>
+
+            <div class="h_70 tarjeta p2 white_bg">
+               <div class="imagen imgLiquid imgLiquidNoFill w_100 h_100">
+                  <?php echo get_the_post_thumbnail(); ?>
+               </div>
             </div>
             <h6 class="texto_enlace fontXS p2 font2 secundario2_bg white text-center tarjeta">
                <?php echo get_the_content(); ?>
             </h6>
+
          </a>
+
       </div>
 
 <?php
